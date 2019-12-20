29da7c4bfffad1e95c455c69410ab188d79c113c
Hui <jeffrey.x.hui@gmail.com>
2019-12-20 08:05:47 -0800

    set up app with dependencies and login wireframe

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../helpers/axiosWithAuth'
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [loginQuery, setLoginQuery] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleLogin = ev => {
+    ev.preventDefault()
+    axiosWithAuth('/login', )
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form onSubmit={handleLogin}>
+        <input
+
+        />
+        <input
+
+        />
+        <button type='submit'>Login</button>
+      </form>
     </>
   );
 };
