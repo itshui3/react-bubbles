1a2890a3909034a167df561f51a3adef43d0d062
Hui <jeffrey.x.hui@gmail.com>
2019-12-20 08:10:02 -0800

    login successful, redirect to bubbles successful

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { axiosWithAuth } from '../helpers/axiosWithAuth'
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const history = useHistory()
+
   const [loginQuery, setLoginQuery] = useState({
     username: '',
     password: ''
@@ -11,18 +14,37 @@ const Login = () => {
 
   const handleLogin = ev => {
     ev.preventDefault()
-    axiosWithAuth('/login', )
+    axiosWithAuth().post('/login', loginQuery)
+      .then( res => {
+        localStorage.setItem('token', res.data.payload)
+        console.log(res)
+        history.push('/bubbles')
+      })
+      .catch( err => {
+        console.log(err)
+      })
   }
+
+  const handleInput = ev => {
+    setLoginQuery({...loginQuery, [ev.target.name]: ev.target.value })
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
       <form onSubmit={handleLogin}>
         <input
-
+          placeholder='username'
+          name='username'
+          value={loginQuery.username}
+          onChange={handleInput}
         />
         <input
-
+          placeholder='password'
+          name='password'
+          value={loginQuery.password}
+          onChange={handleInput}
         />
         <button type='submit'>Login</button>
       </form>
